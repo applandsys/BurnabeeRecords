26724c286a4427547f2342a1157c4bada6cb348b
applandsys <applandsys@gmail.com>
2023-03-08 23:27:06 +0600

    Dashboard sudu graph ta baki ase

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -107,111 +107,16 @@
                                </div>
                                <div class="iq-feature-list">
                                   <h6 class="font-weight-600 mb-0">{{ $topview->title}}</h6>
-                                  <p class="mb-0 mt-2">T.v show</p>
+                                  <p class="mb-0 mt-2">Music</p>
                                   <div class="d-flex align-items-center my-2 iq-ltr-direction">
-                                     <p class="mb-0 mr-2"><i class="lar la-eye mr-1"></i> 134</p>
-                                     <p class="mb-0 "><i class="las la-download ml-2"></i> 30 k</p>
+                                     <p class="mb-0 mr-2"><i class="lar la-eye mr-1"></i> {{$topview->hitcounter->sum('raw_hits')}}</p>
+                                     <!-- <p class="mb-0 "><i class="las la-download ml-2"></i> 30 k</p> -->
                                   </div>
                                </div>
                             </div>
                          </div>
                       </li>
                       @endforeach
-                      <li class="col-sm-6 col-lg-4 col-xl-3 iq-rated-box">
-                         <div class="iq-card mb-0">
-                            <div class="iq-card-body p-0">
-                               <div class="iq-thumb">
-                                  <a href="javascript:void(0)">
-                                     <img src="../assets/images/dashboard/02.jpg" class="img-fluid w-100 img-border-radius" alt="">
-                                  </a>
-                               </div>
-                               <div class="iq-feature-list">
-                                  <h6 class="font-weight-600 mb-0">Last Night</h6>
-                                  <p class="mb-0 mt-2">Movie</p>
-                                  <div class="d-flex align-items-center my-2 iq-ltr-direction">
-                                     <p class="mb-0 mr-2"><i class="lar la-eye mr-1"></i> 133</p>
-                                     <p class="mb-0 "><i class="las la-download ml-2"></i> 20 k</p>
-                                  </div>
-                               </div>
-                            </div>
-                         </div>
-                      </li>
-                      <li class="col-sm-6 col-lg-4 col-xl-3 iq-rated-box">
-                         <div class="iq-card mb-0">
-                            <div class="iq-card-body p-0">
-                               <div class="iq-thumb">
-                                  <a href="javascript:void(0)">
-                                     <img src="../assets/images/dashboard/03.jpg" class="img-fluid w-100 img-border-radius" alt="">
-                                  </a>
-                               </div>
-                               <div class="iq-feature-list">
-                                  <h6 class="font-weight-600 mb-0">Jeon Woochie</h6>
-                                  <p class="mb-0 mt-2">Movie</p>
-                                  <div class="d-flex align-items-center my-2 iq-ltr-direction">
-                                     <p class="mb-0 mr-2"><i class="lar la-eye mr-1"></i> 222</p>
-                                     <p class="mb-0 "><i class="las la-download ml-2"></i> 40 k</p>
-                                  </div>
-                               </div>
-                            </div>
-                         </div>
-                      </li>
-                      <li class="col-sm-6 col-lg-4 col-xl-3 iq-rated-box">
-                         <div class="iq-card mb-0">
-                            <div class="iq-card-body p-0">
-                               <div class="iq-thumb">
-                                  <a href="javascript:void(0)">
-                                     <img src="../assets/images/dashboard/04.jpg" class="img-fluid w-100 img-border-radius" alt="">
-                                  </a>
-                               </div>
-                               <div class="iq-feature-list">
-                                  <h6 class="font-weight-600 mb-0">Dino Land</h6>
-                                  <p class="mb-0 mt-2">T.v show</p>
-                                  <div class="d-flex align-items-center my-2 iq-ltr-direction">
-                                     <p class="mb-0 mr-2"><i class="lar la-eye mr-1"></i> 122</p>
-                                     <p class="mb-0 "><i class="las la-download ml-2"></i> 25 k</p>
-                                  </div>
-                               </div>
-                            </div>
-                         </div>
-                      </li>
-                      <li class="col-sm-6 col-lg-4 col-xl-3 iq-rated-box">
-                         <div class="iq-card mb-0">
-                            <div class="iq-card-body p-0">
-                               <div class="iq-thumb">
-                                  <a href="javascript:void(0)">
-                                     <img src="../assets/images/dashboard/05.jpg" class="img-fluid w-100 img-border-radius" alt="">
-                                  </a>
-                               </div>
-                               <div class="iq-feature-list">
-                                  <h6 class="font-weight-600 mb-0">Last Race</h6>
-                                  <p class="mb-0 mt-2">T.v show</p>
-                                  <div class="d-flex align-items-center my-2 iq-ltr-direction">
-                                     <p class="mb-0 mr-2"><i class="lar la-eye mr-1"></i> 144</p>
-                                     <p class="mb-0 "><i class="las la-download ml-2"></i> 35 k</p>
-                                  </div>
-                               </div>
-                            </div>
-                         </div>
-                      </li>
-                      <li class="col-sm-6 col-lg-4 col-xl-3 iq-rated-box">
-                         <div class="iq-card mb-0">
-                            <div class="iq-card-body p-0">
-                               <div class="iq-thumb">
-                                  <a href="javascript:void(0)">
-                                     <img src="../assets/images/dashboard/06.jpg" class="img-fluid w-100 img-border-radius" alt="">
-                                  </a>
-                               </div>
-                               <div class="iq-feature-list">
-                                  <h6 class="font-weight-600 mb-0">Opend Dead Shot</h6>
-                                  <p class="mb-0 mt-2">T.v show</p>
-                                  <div class="d-flex align-items-center my-2 iq-ltr-direction">
-                                     <p class="mb-0 mr-2"><i class="lar la-eye mr-1"></i> 134</p>
-                                     <p class="mb-0 "><i class="las la-download ml-2"></i> 30 k</p>
-                                  </div>
-                               </div>
-                            </div>
-                         </div>
-                      </li>
                    </ul>
                 </div>
              </div>
@@ -288,8 +193,10 @@
              </div>
           </div>
        </div>
+
+       
        <div class="row">
-          <div class="col-sm-12  col-lg-4">
+          <!-- <div class="col-sm-12  col-lg-4">
              <div class="iq-card iq-card-block iq-card-stretch iq-card-height">
                 <div class="iq-card-header d-flex align-items-center justify-content-between">
                    <div class="iq-header-title">
@@ -300,8 +207,8 @@
                    <div id="view-chart-03"></div>
                 </div>
              </div>
-          </div>
-          <div class="col-lg-8">
+          </div> -->
+          <!-- <div class="col-lg-8">
              <div class="iq-card iq-card-block iq-card-stretch iq-card-height">
                 <div class="iq-card-header d-flex align-items-center justify-content-between">
                    <div class="iq-header-title">
@@ -399,7 +306,7 @@
                    </div>
                 </div>
              </div>
-          </div>
+          </div> -->
           <div class="col-sm-12">
              <div class="iq-card">
                 <div class="iq-card-header d-flex justify-content-between">
@@ -424,113 +331,36 @@
                                <th style="width:10%;">Rating</th>
                                <th style="width:20%;">Category</th>
                                <th style="width:10%;">Download/Views</th>
-                               <th style="width:10%;">User</th>
-                               <th style="width:20%;">Date</th>
+                               <!-- <th style="width:10%;">User</th>
+                               <th style="width:20%;">Date</th> -->
                                <th style="width:10%;"><i class="lar la-heart"></i></th>
                             </tr>
                          </thead>
                          <tbody>
+                         @foreach($top_view_video as $topview)
                             <tr>
                                <td>
                                   <div class="media align-items-center">
                                      <div class="iq-movie">
-                                        <a href="javascript:void(0);"><img src="../assets/images/movie-thumb/01.jpg" class="img-border-radius avatar-40 img-fluid" alt=""></a>
+                                        <a href="javascript:void(0);"><img src="{{ asset('uploads/video/thumbnail')}}/{{$topview->thubmnail_image}}" class="img-border-radius avatar-40 img-fluid" alt=""></a>
                                      </div>
                                      <div class="media-body text-white text-left ml-3">
-                                        <p class="mb-0">Champions</p>
-                                        <small>1h 40m</small>
+                                        <p class="mb-0">{{$topview->title}}</p>
+                                        <small>{{$topview->videodatail->duration}}</small>
                                      </div>
                                   </div>
                                </td>
                                <td><i class="lar la-star mr-2"></i> 9.2</td>
-                               <td>Horror</td>
+                               <td>Music</td>
                                <td>
-                                  <i class="lar la-eye "></i>
+                               {{$topview->hitcounter->sum('raw_hits')}} <i class="lar la-eye "></i>
                                </td>
-                               <td>Unsubcriber</td>
-                               <td>21 July,2020</td>
+                               <!-- <td>Unsubcriber</td>
+                               <td>21 July,2020</td> -->
                                <td><i class="las la-heart text-primary"></i></td>
                             </tr>
-                            <tr>
-                               <td >
-                                  <div class="media align-items-center">
-                                     <div class="iq-movie">
-                                        <a href="javascript:void(0);"><img src="../assets/images/show-thumb/05.jpg" class="img-border-radius avatar-40 img-fluid" alt=""></a>
-                                     </div>
-                                     <div class="media-body text-white text-left ml-3">
-                                        <p class="mb-0">Last Race</p>
-                                     </div>
-                                  </div>
-                               </td>
-                               <td><i class="lar la-star mr-2"></i> 7.2</td>
-                               <td>Horror</td>
-                               <td>
-                                  <i class="lar la-eye "></i>
-                               </td>
-                               <td>subcriber</td>
-                               <td>22 July,2020</td>
-                               <td><i class="las la-heart text-primary"></i></td>
-                            </tr>
-                            <tr>
-                               <td>
-                                  <div class="media align-items-center">
-                                     <div class="iq-movie">
-                                        <a href="javascript:void(0);"><img src="../assets/images/show-thumb/07.jpg" class="img-border-radius avatar-40 img-fluid" alt=""></a>
-                                     </div>
-                                     <div class="media-body text-white text-left ml-3">
-                                        <p class="mb-0">Boop Bitty</p>
-                                     </div>
-                                  </div>
-                               </td>
-                               <td><i class="lar la-star mr-2"></i> 8.2</td>
-                               <td>Thriller</td>
-                               <td>
-                                  <i class="lar la-eye "></i>
-                               </td>
-                               <td>Unsubcriber</td>
-                               <td>23 July,2020</td>
-                               <td><i class="las la-heart text-primary"></i></td>
-                            </tr>
-                            <tr>
-                               <td>
-                                  <div class="media align-items-center">
-                                     <div class="iq-movie">
-                                        <a href="javascript:void(0);"><img src="../assets/images/show-thumb/10.jpg" class="img-border-radius avatar-40 img-fluid" alt=""></a>
-                                     </div>
-                                     <div class="media-body text-white text-left ml-3">
-                                        <p class="mb-0">Dino Land</p>
-                                     </div>
-                                  </div>
-                               </td>
-                               <td><i class="lar la-star mr-2"></i> 8.5</td>
-                               <td>Action</td>
-                               <td>
-                                  <i class="lar la-eye "></i>
-                               </td>
-                               <td>Unsubcriber</td>
-                               <td>24 July,2020</td>
-                               <td><i class="las la-heart text-primary"></i></td>
-                            </tr>
-                            <tr>
-                               <td>
-                                  <div class="media align-items-center">
-                                     <div class="iq-movie">
-                                        <a href="javascript:void(0);"><img src="../assets/images/show-thumb/04.jpg" class="img-border-radius avatar-40 img-fluid" alt=""></a>
-                                     </div>
-                                     <div class="media-body text-white text-left ml-3">
-                                        <p class="mb-0">The Last Breath</p>
-                                     </div>
-                                  </div>
-                               </td>
-                               <td><i class="lar la-star mr-2"></i> 8.9</td>
-                               <td>Horror</td>
-                               <td>
-                                  <i class="lar la-eye "></i>
-                               </td>
-                               <td>subcriber</td>
-                               <td>25 July,2020</td>
-                               <td><i class="las la-heart text-primary"></i></td>
-                            </tr>
+                         @endforeach
+                    
                          </tbody>
                       </table>
                    </div>
